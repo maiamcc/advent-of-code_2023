fa22ca24b0c860bffd5a90eafbfbfb06a61fcd28
Maia <maia.mcc@gmail.com>
2023-12-01 19:47:21 -0500

    day 1 part 1

--- day1/day1.go
+++ day1/day1.go
@@ -4,39 +4,42 @@ import (
 	"fmt"
 	"github.com/maiamcc/advent-of-code_2023/utils"
 	"strconv"
-	"testing"
+	"unicode"
 )
 
 func main() {
 	inputLines := utils.MustReadFileAsLines("day1/input.txt")
+	fmt.Println("The answer to Part One is:", partOne(inputLines))
+	fmt.Println("The answer to Part Two is:", partTwo(inputLines))
+}
 
+func partOne(inputLines []string) int {
 	total := 0
 	for i, ln := range inputLines {
-		numStr := fmt.Sprintf("%s%s", ln[0], ln[len(ln)-1])
+		numChars := getNumericChars(ln)
+		numStr := fmt.Sprintf("%s%s", string(numChars[0]), string(numChars[len(numChars)-1]))
 		num, err := strconv.Atoi(numStr)
 		if err != nil {
 			utils.LogfErrorAndExit(err, "converting ln at index %d ('%s')", i, ln)
 		}
 		total += num
 	}
-	fmt.Printf("Total: %d", total)
+	return total
 }
 
 // getNumericChars returns an array of all the numeric characters in given string s
 // (still stored as strings))
 func getNumericChars(s string) []string {
-
+	res := []string{}
+	for _, ch := range s {
+		if unicode.IsNumber(ch) {
+			res = append(res, string(ch))
+		}
+	}
+	return res
 }
 
-func TestGetNumericChars(t *testing.T) {
-	cases := map[string][]string{ // map input to expected output
-		"1hello2":      {"1", "2"},
-		"hel1lo2world": {"1", "2"},
-		"321stuff":     {"3", "2", "1"},
-	}
-
-	for k, v := range cases {
-		actual := getNumericChars(k)
-		assert.Equal()
-	}
+func partTwo(inputLines []string) int {
+	//total := 0
+	return len(inputLines)
 }
--- day1/day1_test.go
+++ day1/day1_test.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGetNumericChars(t *testing.T) {
+	cases := map[string][]string{ // map input to expected output
+		"1hello2":      {"1", "2"},
+		"hel1lo2world": {"1", "2"},
+		"321stuff":     {"3", "2", "1"},
+		"nonumbers":    {},
+		"123":          {"1", "2", "3"},
+	}
+
+	for input, expected := range cases {
+		actual := getNumericChars(input)
+		assert.Equal(t, expected, actual, "for input: %s", input)
+	}
+}
