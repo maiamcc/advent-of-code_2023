f60d6693a9b9e818ab31f053c845aab609d17be9
Maia <maia.mcc@gmail.com>
2023-12-02 14:15:26 -0500

    helpers

--- day2/day2.go
+++ day2/day2.go
@@ -3,7 +3,14 @@ package main
 import (
 	"fmt"
 	"github.com/maiamcc/advent-of-code_2023/utils"
+	"regexp"
+	"strings"
 )
+
+var redRe = regexp.MustCompile("(\\d+) red")
+var greenRe = regexp.MustCompile("(\\d+) green")
+var blueRe = regexp.MustCompile("(\\d+) blue")
+var gameIdRe = regexp.MustCompile("Game (\\d+)")
 
 func main() {
 	inputLines := utils.MustReadFileAsLines("day2/input.txt")
@@ -17,4 +24,60 @@ func partOne(inputLines []string) int {
 
 func partTwo(inputLines []string) int {
 	return len(inputLines)
+}
+
+type round struct {
+	numRed   int
+	numGreen int
+	numBlue  int
+}
+
+func (rnd round) isEmpty() bool {
+	return rnd.numRed == 0 && rnd.numBlue == 0 && rnd.numGreen == 0
+}
+
+type game struct {
+	id     int
+	rounds []round
+}
+
+func numResultFromRe(input string, re *regexp.Regexp) int {
+	res := re.FindStringSubmatch(input)
+	if len(res) == 0 {
+		// no match
+		return 0
+	}
+	return utils.MustAtoI(res[1])
+}
+func parseRound(input string) round {
+	return round{
+		numRed:   numResultFromRe(input, redRe),
+		numGreen: numResultFromRe(input, greenRe),
+		numBlue:  numResultFromRe(input, blueRe),
+	}
+}
+
+func parseGame(input string) (game, error) {
+	parts := strings.Split(input, ":")
+	if len(parts) != 2 {
+		return game{}, fmt.Errorf("unexpected num parts when splitting input string '%s' on colon", input)
+	}
+	gameId := numResultFromRe(parts[0], gameIdRe)
+	if gameId == 0 {
+		return game{}, fmt.Errorf("couldn't parse game id from input string '%s", input)
+	}
+	roundStrs := strings.Split(parts[1], ";")
+	var rounds []round
+	for _, rnd := range roundStrs {
+		round := parseRound(rnd)
+		if round.isEmpty() {
+			return game{}, fmt.Errorf("couldn't parse round from input string '%s", rnd)
+		}
+		rounds = append(rounds, round)
+
+	}
+	return game{
+		id:     gameId,
+		rounds: rounds,
+	}, nil
 }
--- day2/day2_test.go
+++ day2/day2_test.go
@@ -13,3 +13,50 @@ func TestPartOne(t *testing.T) {
 	actual := partOne(inputLns)
 	assert.Equal(t, 8, actual)
 }
+
+func TestParseRound(t *testing.T) {
+	cases := map[string]round{ // map input to expected output
+		"5 red":                        {5, 0, 0},
+		"5 green":                      {0, 5, 0},
+		"5 blue":                       {0, 0, 5},
+		"1 blue, 2 green, 3 red":       {3, 2, 1},
+		"6 red, 9 blue":                {6, 0, 9},
+		"123 green, 456 red, 789 blue": {456, 123, 789},
+		"bad string":                   {0, 0, 0},
+	}
+
+	for input, expected := range cases {
+		t.Run(input, func(t *testing.T) {
+			actual := parseRound(input)
+			assert.Equal(t, expected, actual)
+		})
+	}
+}
+
+func TestParseGame(t *testing.T) {
+	cases := map[string]game{ // map input to expected output
+		"Game 1: 5 red": {1, []round{{5, 0, 0}}},
+		"Game 2: 5 green; 5 blue": {2, []round{
+			{0, 5, 0},
+			{0, 0, 5}}},
+		"Game 3: 1 blue, 2 green, 3 red; 3 blue, 2 green, 1 red; 1 blue, 1 green, 1 red": {3,
+			[]round{
+				{3, 2, 1},
+				{1, 2, 3},
+				{1, 1, 1}}},
+		"Game 4: 6 red; 9 blue, 5 red; 6 green": {4,
+			[]round{
+				{6, 0, 0},
+				{5, 0, 9},
+				{0, 6, 0},
+			}},
+	}
+
+	for input, expected := range cases {
+		t.Run(input, func(t *testing.T) {
+			actual, err := parseGame(input)
+			assert.Nil(t, err)
+			assert.Equal(t, expected, actual)
+		})
+	}
+}
