adef9042eb8df751ac87a3ce485c594d990caf61
Maia <maia.mcc@gmail.com>
2023-12-11 22:09:45 -0500

    ughhhh day8 pt2

--- day8/day8.go
+++ day8/day8.go
@@ -8,36 +8,59 @@ import (
 	"strings"
 )
 
-var nodeRe = regexp.MustCompile("([A-Z]{3}) = \\(([A-Z]{3}), ([A-Z]{3})\\)")
+var nodeRe = regexp.MustCompile("([A-Z0-9]{3}) = \\(([A-Z0-9]{3}), ([A-Z0-9]{3})\\)")
 
 func main() {
 	fullInput := utils.MustReadFileAsString("day8/input.txt")
 	fmt.Println("The answer to Part One is:", partOne(fullInput))
-	//fmt.Println("The answer to Part Two is:", partTwo(fullInput))
+	fmt.Println("The answer to Part Two is:", partTwo(fullInput))
 }
 
 func partOne(fullInput string) int {
-	dirs, nodeMap := parseInput(fullInput)
+	dirs, nm := parseInput(fullInput)
 
-	curNode, ok := nodeMap["AAA"]
-	if !ok {
-		utils.LogfAndExit("couldn't find start node")
+	count, _ := stepsToEndNodes(dirs, nm, "AAA", func(n *node) bool {
+		return n.label == "ZZZ"
+	})
+	return count
+}
+
+func partTwo(fullInput string) int {
+	dirs, nodes := parseInput(fullInput)
+
+	startNodes := nodes.findStartNodes()
+	var stepCounts []int
+	for _, startNode := range startNodes {
+		numSteps, _ := stepsToEndNodes(dirs, nodes, startNode.label, func(n *node) bool {
+			return n.isTerminalNode()
+		})
+		// Look, this is janky but we can use the lcm of the number of steps because for
+		// every nodeA -> nodeZ in n steps, nodeZ -> nodeZ cycles in n steps as well
+		// (which isn't a constraint in the problem but is a constraint I found in my
+		// input, grumble grumble
+		stepCounts = append(stepCounts, numSteps)
 	}
+	lcm := LCM(stepCounts[0], stepCounts[1], stepCounts[2:]...)
+	return lcm
+}
+
+func stepsToEndNodes(dirs []string, nodes nodeMap, startNodeLabel string, endCond func(n *node) bool) (int, *node) {
+	curNode, ok := nodes[startNodeLabel]
+	if !ok {
+		utils.LogfAndExit("couldn't find start node %s", startNodeLabel)
+	}
+
 	count := 0
 	for {
 		for _, dir := range dirs {
 			curNode = curNode.step(dir)
 			count += 1
-			if curNode.label == "ZZZ" {
+			if endCond(curNode) {
 				// win condition
-				return count
+				return count, curNode
 			}
 		}
 	}
-}
-
-func partTwo(fullInput string) int {
-	return len(fullInput)
 }
 
 type node struct {
@@ -54,6 +77,16 @@ func (n *node) step(dir string) *node {
 	}
 	utils.LogfAndExit("Unrecognized direction string '%s'\n", dir)
 	return nil
+}
+
+func (n *node) isTerminalNode() bool {
+	// is this too expensive? can cache it.
+	return string(n.label[len(n.label)-1]) == "Z"
+}
+
+func (n *node) isStartNode() bool {
+	// is this too expensive? can cache it.
+	return string(n.label[len(n.label)-1]) == "A"
 }
 
 func parseNode(input string, nodeMap map[string]*node) {
@@ -99,10 +132,56 @@ func parseNodes(input string) map[string]*node {
 	return nodeMap
 }
 
-func parseInput(input string) (directions []string, nodeMap map[string]*node) {
+func parseInput(input string) (directions []string, nm nodeMap) {
 	dirsAndNodes, err := utils.SplitIntoExpectedParts(input, "\n\n", 2)
 	if err != nil {
 		utils.LogfErrorAndExit(err, "splitting input")
 	}
 	return strings.Split(strings.TrimSpace(dirsAndNodes[0]), ""), parseNodes(strings.TrimSpace(dirsAndNodes[1]))
+}
+
+// blurgh nodeMap should be its own type alias
+
+type nodeMap map[string]*node
+
+func (nm nodeMap) findStartNodes() []*node {
+	var res []*node
+	for _, n := range nm {
+		if n.isStartNode() {
+			res = append(res, n)
+		}
+	}
+	return res
+}
+
+func (nm nodeMap) findEndNodes() []*node {
+	var res []*node
+	for _, n := range nm {
+		if n.isTerminalNode() {
+			res = append(res, n)
+		}
+	}
+	return res
+}
+
+// Shamelessly stolen from https://siongui.github.io/2017/06/03/go-find-lcm-by-gcd/
+// greatest common divisor (GCD) via Euclidean algorithm
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+// find Least Common Multiple (LCM) via GCD
+func LCM(a, b int, integers ...int) int {
+	result := a * b / GCD(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = LCM(result, integers[i])
+	}
+
+	return result
 }
--- day8/day8_test.go
+++ day8/day8_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"github.com/maiamcc/advent-of-code_2023/utils"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -13,10 +15,23 @@ func TestPartOne(t *testing.T) {
 }
 
 func TestPartTwo(t *testing.T) {
-	inputStr := "input\ngoes\nhere"
+	inputStr := "LR\n\n11A = (11B, XXX)\n11B = (XXX, 11Z)\n11Z = (11B, XXX)\n22A = (22B, XXX)\n22B = (22C, 22C)\n22C = (22Z, 22Z)\n22Z = (22B, 22B)\nXXX = (XXX, XXX)\n"
 
 	actual := partTwo(inputStr)
-	assert.Equal(t, 123, actual)
+	assert.Equal(t, 6, actual)
+}
+
+func TestStartEndNodes(t *testing.T) {
+	dirs, nodes := parseInput(utils.MustReadFileAsString("input.txt"))
+	startNodes := nodes.findEndNodes()
+	var endNodes []*node
+	for _, n := range startNodes {
+		count, endNode := stepsToEndNodes(dirs, nodes, n.label, func(n *node) bool {
+			return n.isTerminalNode()
+		})
+		endNodes = append(endNodes, endNode)
+		fmt.Printf("%s --> %s in %d steps\n", n.label, endNode.label, count)
+	}
 }
 
 func TestParseNodes(t *testing.T) {
